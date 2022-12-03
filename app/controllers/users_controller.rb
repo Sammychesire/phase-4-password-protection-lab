c92db3b3d2f5f66245ecc54dd0b5419025629577
bob <sammy.chesire@student.moringaschool.com>
2022-12-03 13:56:00 +0300

    complete assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user, status: :ok
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: "Unaouthorized"}, status: 401
+        
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
